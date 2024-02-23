5a28ed8be7fd1aa6fa826adcaa63c3c373d896ac
HiroyukiShimabukuro <62634476+HiroyukiShimabukuro@users.noreply.github.com>
2024-02-23 02:48:31 +0000

    fix `page status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,4 @@
-import database from "../../../../infra/database.js";
-
 async function status(request, response) {
-  const result = database.query("SELECT 1+1 as SUM;");
-  console.log(result);
   response.status(200).json({ chave: "são acima da média" });
 }
 
