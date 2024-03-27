ab5d813a65b52bf76b7403e6e079cb7351ec46da
Hiroyuki Shimabukuro <hiro.carlos93@gmail.com>
2024-03-26 22:47:22 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,29 @@ import database from "infra/database.js";
 async function status(request, response) {
 
   const updatedAt = new Date().toISOString();
-  const result = await database.query('SELECT VERSION();'); 
-  
-  const version = result.rows[0].version
 
-  const connections = await database.pool();
-  
+  const databaseVersionResult = await database.query('SHOW server_version;'); 
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query('SHOW max_connections;'); 
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  }); 
+    
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
   response.status(200).json({
     updated_at : updatedAt,
-    database_version : version,
-    max_connections: connections.options.max,
-    connections: connections.totalCount
+    dependencies : {
+      database: {
+        version : databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue)
+      }
+    }
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,8 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.database_version).toEqual("PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit");
-  expect(responseBody.max_connections).toEqual(20);
-  expect(responseBody.connections).toEqual(1);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(3);
 });
+
